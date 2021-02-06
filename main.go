21a8143074b5761224d0d907a3fdccbfac614255
Zack Jones <zackjones11@gmail.com>
2021-02-06 21:36:22 +0100

    create a http connection on port 3000 and upgrade to WS protocol

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/zackjones11/walky-talky-server/pkg/websocket"
+)
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	_, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+}
+
+
+func setupRoutes() {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r)
+	})
+}
+
 func main() {
+	setupRoutes()
+	http.ListenAndServe(":3000", nil)
 
 }
---
+++ pkg/websocket/websocket.go
@@ -0,0 +1,26 @@
+package websocket
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// Upgrade the connection to a websocket protocol using websocket.Upgrader
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return conn, nil
+}
